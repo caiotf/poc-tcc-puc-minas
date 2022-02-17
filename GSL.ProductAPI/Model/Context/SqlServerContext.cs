7f5658cf207319e59d5ccbd4316c9dbf682987c5
Caio Teixeira de Farias <caiotfgv@hotmail.com>
2022-02-17 15:40:06 -0300

    Add project files.

---
+++ GSL.ProductAPI/Model/Context/SqlServerContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GSL.ProductAPI.Model.Context
+{
+    public class SqlServerContext : DbContext
+    {
+        public SqlServerContext()
+        {
+        }
+        public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ GSL.ProductAPI/Startup.cs
@@ -0,0 +1,55 @@
+using GSL.ProductAPI.Model.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace GSL.ProductAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration["SqlServerConnection:SqlServerConnectionString"];
+
+            services.AddDbContext<SqlServerContext>(options => options.UseSqlServer(connection));
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GSL.ProductAPI", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GSL.ProductAPI v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
