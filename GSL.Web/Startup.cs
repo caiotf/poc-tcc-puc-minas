745f286a32544735548711f471982f51d85be970
Caio Teixeira de Farias <caiotfgv@hotmail.com>
2022-03-20 16:02:16 -0300

    adicionando autenticação keycloak no projeto web

--- GSL.Web/Startup.cs
+++ GSL.Web/Startup.cs
@@ -1,10 +1,15 @@
 using GSL.Web.Services;
 using GSL.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 using System;
 
 namespace GSL.Web
@@ -25,6 +30,53 @@ namespace GSL.Web
                 c.BaseAddress = new Uri(Configuration["ServiveUrls:ProductAPI"]));
 
             services.AddControllersWithViews();
+
+            string _clientId = Configuration["SsoKeycloak:clientId"];
+            string _clientSecret = Configuration["SsoKeycloak:clientSecret"];
+            string _redirectUri = Configuration["SsoKeycloak:redirectUri"];
+            string _tenant = Configuration["SsoKeycloak:tenant"];
+            string _authority = String.Format(System.Globalization.CultureInfo.InvariantCulture,
+                Configuration["SsoKeycloak:authority"], _tenant);
+            string _metadataAddress = _authority + "/.well-known/openid-configuration";
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            {
+                //Use default signin scheme
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                //Keycloak server
+                options.Authority = _authority;
+                //Keycloak client ID
+                options.ClientId = _clientId;
+                //Keycloak client secret
+                options.ClientSecret = _clientSecret;
+                //Keycloak .wellknown config origin to fetch config
+                options.MetadataAddress = _metadataAddress;
+                //Require keycloak to use SSL
+                options.RequireHttpsMetadata = false; //false for development only
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                //Save the token
+                options.SaveTokens = true;
+                //Token response type, will sometimes need to be changed to IdToken, depending on config.
+                options.ResponseType = OpenIdConnectResponseType.Code;
+                //SameSite is needed for Chrome/Firefox, as they will give http error 500 back, if not set to unspecified.
+                options.NonceCookie.SameSite = SameSiteMode.Unspecified;
+                options.CorrelationCookie.SameSite = SameSiteMode.Unspecified;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                    RoleClaimType = "groups",
+                    ValidateIssuer = true
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +94,7 @@ namespace GSL.Web
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
