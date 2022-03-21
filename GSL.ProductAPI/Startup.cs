918c2b71f19f08c286c7b278ae1f6898160b3e2a
Caio Teixeira de Farias <caiotfgv@hotmail.com>
2022-03-20 22:24:02 -0300

    adicionada autorização no swagger

--- GSL.ProductAPI/Startup.cs
+++ GSL.ProductAPI/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
 
 namespace GSL.ProductAPI
 {
@@ -42,6 +43,32 @@ namespace GSL.ProductAPI
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GSL.ProductAPI", Version = "v1" });
+                c.EnableAnnotations();
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"Enter 'Bearer' [space] and your token!",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                             Type = ReferenceType.SecurityScheme,
+                             Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string> ()
+                    }
+                });
             });
 
             string authority = Configuration.GetValue<string>("Authority");
