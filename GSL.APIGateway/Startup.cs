b648923f4711bd40a532e030c14e0803666ee204
Caio Teixeira de Farias <caiotfgv@hotmail.com>
2022-05-10 18:20:14 -0300

    add swagger to ocelot

--- GSL.APIGateway/Startup.cs
+++ GSL.APIGateway/Startup.cs
@@ -1,10 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+using MMLib.Ocelot.Provider.AppConfiguration;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -22,39 +21,32 @@ namespace GSL.APIGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddOcelot(Configuration)
+               .AddAppConfiguration();
+            services.AddSwaggerForOcelot(Configuration);
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GSL.APIGateway", Version = "v1" });
-            });
-            services.AddOcelot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GSL.APIGateway v1"));
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+
+            app.UseSwaggerForOcelotUI(opt =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                opt.PathToSwaggerGenerator = "/swagger/docs";
+
             });
-            await app.UseOcelot();
+            app.UseOcelot()
+               .Wait();
         }
     }
 }
